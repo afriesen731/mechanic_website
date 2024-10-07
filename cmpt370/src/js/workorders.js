22036914c2918c8b47029d258c8c1dd5c62b0abd
Alex Friesen (mog136) <mog136@mail.usask.ca>
2024-10-06 22:24:59 -0600

    started FilteredDataset class

--- cmpt370/src/js/workorders.js
+++ cmpt370/src/js/workorders.js
@@ -2,26 +2,29 @@ import PocketBase from 'pocketbase';
 // PocketBase SDK initialization
 const pb = new PocketBase('http://ddmpmc.duckdns.org:8090');
 
-
-
 const table = document.getElementById('table');
-
+const DEFAULT_PAGE_LEN = 50;
 
 
 
 
 
 class SetFilter {
-    constructor(isFiltered=false, set={}) {
+    constructor(column, isFiltered=false, set=[]) {
+        this.column = column;
         this.isFiltered = isFiltered;
         this.set = set;
         
     }
 
+
+    this
+
 }
 
 class RangeFilter {
-    constructor(isFiltered=false, start=null, end=NULL) {
+    constructor(column, isFiltered=false, start=null, end=null) {
+        this.column = column;
         this.isFiltered = isFiltered;
         this.start = start;
         this.end = end;
@@ -30,52 +33,138 @@ class RangeFilter {
 
 
 
-class Table {
-    constructor() {
-        this.page = 1;
-        this.pageLen = 50;
-        // TODO: find out how many pages there are and dont allow to go max page len
+class FilteredDataset {
+    constructor(dataset) {        
+        this.dataset = dataset;
+        this.pageLen = DEFAULT_PAGE_LEN;
+        this._page = 1;
         this.totalPages = 0;
-        this.date = new RangeFilter();
-        this.mechanics = new SetFilter();
-        this.license = new SetFilter();
-        this.serviceType = new SetFilter();
-        this.status = new SetFilter();
-        this.items = NULL;
-        this.update();
+        this.items = null;
+        this.filters = [];
     }
 
-    update() {
+    addSetFilter(column, isSet=false, set=[]) {
+        let newfilter = new SetFilter(column, isSet, set);
+        this.filters.push(newfilter);
+
+    }
+
+    addRangeFilter(column, isSet=false, start=null, end=null) {
+        let newfilter = new RangeFilter(column, isSet, start, end);
+        this.filters.push(newfilter);
         
+    }
 
-        let result = pb.collections('work_orders').getList(this.page, this.pageLen, {
-            
-            // TODO: make it so that it adds the filters
-            filter: filter
 
-        }).then(response => {
-            this.items = response.items;
+    getfilter(column) {
+        this.filters.forEach(filter => {
+            if (filter.column == column) {
+                return filter;
+            }
 
-        }).catch(error => {
-            console.error('Error fetching work orders:', error);
 
         });
 
+        return null
     }
 
 
 
-    // TODO: add paging functions
-    nextPage() {
+    async update() {
+        
+        let filter = this.getFilters();
+        
+        try {
 
+            // fetch dataset
+            const response = await pb.collection('work_orders').getList(this._page, this.pageLen, {
+                filter: filter
+            });
+            this.items = response.items;
+
+            // update the total page number
+            this.totalPages = Math.ceil(response.totalItems / this.pageLen);
+        } 
+        catch (error) {
+            console.error('Error fetching work orders:', error);
+        }
+
+    }
+
+
+    getFilters() {
+        let result = [];
+
+        // combine each of the filters into a string
+        this.filters.forEach(filter => {
+            if (filter.isFiltered) {
+
+                // create filter for set filter
+                if (filter instanceof SetFilter) {
+                    
+                    let setValues = Object.values(filter.set)
+                                    .map(value => `"${value}"`).join(',');
+                    result.push(`${filter.column} IN [${setValues}]`);
+                }
+
+                // create filter for range filter
+                else if (filter instanceof RangeFilter) {
+                    if (filter.start !== null && filter.end !== null) {
+                        result.push(`${filter.column} >= ${filter.start} AND ${filter.name} <= ${filter.end}`);
+                    } 
+                    else if (filter.start !== null) {
+                        result.push(`${filter.column} >= ${filter.start}`);
+                    } 
+                    else if (filter.end !== null) {
+                        result.push(`${filter.column} <= ${filter.end}`);
+                    }
+                }
+            }
+
+
+
+        });
+
+        // return the filter as a string
+        if ( result.length > 0 ) {
+            result = '';
+        }
+        else {
+            result = result.join(' AND ');
+        }
+        
+        return result;
+
+
+    }
+
+
+
+    nextPage() {
+        // check if page number is too large
+        if (this._page >= this.totalPages) {
+            return;
+        }
+        this._page++;
+        this.update();
     }
 
     prevPage() {
-
+        // check if page number is too small
+        if (this._page <= 1) {
+            return;
+        }
+        this._page--;
+        this.update();
     }
 
     setPage(pageNumber) {
-
+        // check if page number is in proper range
+        if (pageNumber < 1 && pageNumber >= this.totalPages) {
+            return;
+        }
+        this._page = pageNumber;
+        this.update();
     }
 
 }
@@ -95,10 +184,24 @@ function addRow(date, mechanics, license, serviceType, status) {
 }
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", async function() {
 
     // TODO: set up table
 
+    const authData = await pb.collection('users')
+                                .authWithPassword('password', 'password');
+    console.log(authData);
+    let tableData = new FilteredDataset('work_orders');
+    await tableData.update();
+    console.log("first page:");
+    console.log(tableData.items);
 
-}
+    tableData.getfilter("unit_number").set.append("11111");
+    tableData.update();
+    console.log("unit number: 11111");
+    console.log(tableData.items);
+
+
+
+});
 
