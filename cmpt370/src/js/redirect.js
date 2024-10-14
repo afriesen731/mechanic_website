9c12acec82bc916c53c66144811f67ad8524f83c
Alex Friesen (mog136) <mog136@mail.usask.ca>
2024-10-14 15:27:03 -0600

    finished table

---
+++ cmpt370/src/js/redirect.js
@@ -0,0 +1,8 @@
+import PocketBase from 'pocketbase';
+// PocketBase SDK initialization
+const pb = new PocketBase('http://ddmpmc.duckdns.org:8090');
+
+/* TODO: check if the current user is logged in */
+function isLoggedIn() {
+    
+}
---
+++ cmpt370/src/js/table.js
@@ -0,0 +1,68 @@
+
+
+
+
+/**
+ * Creates a table that can observe a FilteredDataset.
+ */
+class Table {
+    /**
+     * Createst an instance of Table.
+     * @param {HTMLElement} table html element with tag "table".
+     * @param {Array<String>} columns List of columns in the database to display. 
+     *                                  In the order they should be displayed.
+     */
+    constructor(table, columns) {
+        this.columns = columns;
+        this.table = table;
+    }
+
+
+    /**
+     * Updates the contents of the table.
+     * @param {FilteredDataset} dataset A dataset containing all of the information for the table.
+     */
+    update(dataset) {
+        let i;
+        let tbody = document.createElement('tbody');
+        
+        // build the table row by row
+        for (i=0; i < dataset.pageLen; i++) {
+            tbody.appendChild(
+                this.buildRow(dataset.dataset[i], this.columns)
+            );
+        }
+
+        // remove the old table body if it exists
+        if (this.table.querySelector('tbody') != null) {
+            this.table.removeChild(
+                this.table.querySelector('tbody')
+            );
+        }
+
+        this.table.appendChild(tbody);
+
+
+    }
+
+    /**
+     * Creates a row for the table
+     * @param {Dictionary} row A row of a filtered dataset from the database.
+     * @returns {HTMLElement} The 'td' element for a new row.
+     */
+    buildRow(row={}) {
+        return new HTMLElement();
+    }
+
+
+
+    
+
+
+
+}
+
+
+
+
+
